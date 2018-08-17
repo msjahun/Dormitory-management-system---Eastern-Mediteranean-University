d940ea83bcd7bee2cda8584a56e0075dbb9814bd
Musa Jahun <msjahun@live.com>
2018-08-18 01:17:35 +0300

    moved the getDormitoriesByIdandLang to the DormitoryService

--- Dau.Services/Dormitory/DormitoryService.cs
+++ Dau.Services/Dormitory/DormitoryService.cs
@@ -7,8 +7,37 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Dau.Services.Dormitory
 {
-   public class DormitoryService
+   public class DormitoryService : IDormitoryService
     {
-     
+        private fees_and_facilitiesContext _context = new fees_and_facilitiesContext();
+
+        public List<string> GetListAllDormitoriesByLangAndType (int _dormitory_type_id, int _lang_id)
+        {
+            List<string> listDormitories = new List<string>();
+            using (var context = _context)
+            {
+                var dormitories = context.DormitoriesTable
+                                    .Include(dormitory_trans => dormitory_trans.DormitoriesTableTranslation)
+                                    .Include(dormitory_room => dormitory_room.RoomTable)
+
+                                    .ToList();
+
+
+
+                context.DormitoriesTable.Where(d => d.DormitoryTypeId == _dormitory_type_id).ToList().ForEach(dorm =>
+                {
+                    dorm.DormitoriesTableTranslation.Where(r => r.LanguageId == _lang_id).ToList().ForEach(dorm_trans =>
+                    {
+                        listDormitories.Add(dorm_trans.DormitoryName);
+                    });
+
+
+                });
+
+            }
+
+            return listDormitories;
+        }
+
     }
 }
