215f1d9e5ca38bc0832a473e7d5770c87fd0b440
Musa Jahun <msjahun@live.com>
2018-08-18 01:15:29 +0300

    Added IDormitoryService Interface and ISearchService interface in an attempt to prepare my repositories for dependency injection and Inverison of control

---
+++ Dau.Services/Dormitory/IDormitoryService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Dau.Services.Dormitory
+{
+    public interface IDormitoryService
+    {
+        List<string> GetListAllDormitoriesByLangAndType(int _dormitory_type_id, int _lang_id);
+    }
+}
---
+++ Dau.Services/SearchService/ISearchService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Dau.Services.SearchService
+{
+    public interface ISearchService
+    {
+        List<SearchResultData> Filtering(List<SearchResultData> query, PostedDormitoryFilters filter, ArrayList sa);
+        List<SearchResultData> Filtering(List<SearchResultData> query, PostedDormitoryFiltersResponsive filter, ArrayList sa);
+        List<SearchResultData> GetSearchData(int langIDPosted);
+        List<SearchResultData> SortSeachResult(List<SearchResultData> query, string _sort_by);
+    }
+}
