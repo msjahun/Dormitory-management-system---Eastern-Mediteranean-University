c9becc4ff0044d0c6c6c14a8dade8f790aa5fe12
Musa Jahun <msjahun@live.com>
2018-08-18 09:08:09 +0300

    Added sharedResources and tested it

---
+++ searchDormWeb/Resources/SharedResources.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace searchDormWeb
+{
+    public class SharedResources
+    {
+    }
+}
