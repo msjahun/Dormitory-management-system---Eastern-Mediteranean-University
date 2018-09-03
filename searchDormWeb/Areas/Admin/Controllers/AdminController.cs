2fdf09f80b10406d8d08d9a980f1b687601d4279
Musa Jahun <msjahun@live.com>
2018-09-03 19:23:04 +0300

    added some logic that will decide if a user is logged in and be directed to dashboard or redirected to login page if not logged in

--- searchDormWeb/Areas/Admin/Controllers/AdminController.cs
+++ searchDormWeb/Areas/Admin/Controllers/AdminController.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Dau.Core.Domain.User;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 
@@ -13,17 +15,27 @@ namespace searchDormWeb.Areas.Admin.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly SignInManager<User> _signInManager;
+
+        public AdminController(SignInManager<User> signInManager)
+        {
+            _signInManager = signInManager;
+        }
         // GET: Admin
-        [Authorize]
+        //[Authorize]
         [DisplayName("AdminAreaRedirect")]
         public ActionResult Index()
         {
 
-
-           
+            if(_signInManager.IsSignedIn(User))
                 return RedirectToAction("", "Dashboard", new { Area = "Admin" });
            
+                return RedirectToAction("", "Login");
+            
           
+
+
+
         }
     }
 }
