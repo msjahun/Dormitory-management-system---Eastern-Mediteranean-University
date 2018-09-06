328bf0e0e950ab3c9c0a7eb45c7c18bf30959edf
Musa Jahun <msjahun@live.com>
2018-09-06 20:16:05 +0300

    Created debugController for mananging debuging activities

---
+++ searchDormWeb/Areas/Admin/Controllers/DebugController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace searchDormWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]")]
+    [Authorize]
+    public class DebugController : Controller
+    {
+        [HttpGet("[action]")]
+        public IActionResult TagHelpers()
+        {
+            return View();
+        }
+    }
+}
