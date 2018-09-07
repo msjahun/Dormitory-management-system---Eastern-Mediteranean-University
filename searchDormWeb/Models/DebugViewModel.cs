e4bbfde46d57981c191235418c6ddaf0c6b77c16
Musa Jahun <msjahun@live.com>
2018-09-07 15:24:37 +0300

    added more fields, for testing custom taghelpers functionalities

--- searchDormWeb/Models/DebugViewModel.cs
+++ searchDormWeb/Models/DebugViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,8 +13,24 @@ namespace searchDormWeb.Models
         [Required, EmailAddress, MaxLength(256), Display(Name = "Email Address", Description = "Field for valid email addresses")]
         public string Email { get; set; }
 
-        [Required, MinLength(6), MaxLength(50), DataType(DataType.Password), Display(Name = "Password", Description ="Password field")]
+        [MinLength(6), MaxLength(50), DataType(DataType.Password), Display(Name = "Password", Description ="Password field")]
+        
         public string Password { get; set; }
+
+       [Required, Display(Description ="Read only text")]
+        public string TextReadOnly { get; set; }
+
+
+        [Required, Display(Description = "Read only text")]
+        public string TextReadOnlyNotRequired { get; }
+
+
+
+        [Required, EmailAddress, MaxLength(256), Display(Name = "Text area", Description = "Field fortext area addresses")]
+        public string TextArea { get; set; }
+
+        [Required, EmailAddress, MaxLength(256), Display(Name = "Textarea Editor", Description = "Field fortext area addresses")]
+        public string TextAreaEditor { get; set; }
 
         [Display(Name = "Remember Me")]
         public bool RememberMe { get; set; }
