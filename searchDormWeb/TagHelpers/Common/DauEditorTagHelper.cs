59bca5aff67cbed2ad1a7f169e97894be7be6d84
Musa Jahun <msjahun@live.com>
2018-09-07 15:24:56 +0300

    Added a new TagHelper EditorTagHelper

---
+++ searchDormWeb/TagHelpers/Common/DauEditorTagHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace searchDormWeb.TagHelpers.Common
+{
+    public class DauEditorTagHelper : TextAreaTagHelper
+    {
+        public DauEditorTagHelper(IHtmlGenerator generator) : base(generator)
+        {
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "textarea";
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+           
+            output.Attributes.Add("id", For.Name + "Id");
+        
+            
+        }
+    }
+}
