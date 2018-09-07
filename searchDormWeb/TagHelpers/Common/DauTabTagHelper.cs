0e8f25a6a32162a718a775c6e5dd60ef36d3c877
Musa Jahun <msjahun@live.com>
2018-09-08 02:32:48 +0300

    created the subTabTaghelper whose parent id dau-tabs

---
+++ searchDormWeb/TagHelpers/Common/DauTabTagHelper.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace searchDormWeb.TagHelpers.Common
+{
+    [HtmlTargetElement("dau-tab", ParentTag = "dau-tabs")]
+    public class DauTabTagHelper : TagHelper
+    {
+        public string Name { get; set; }
+        public string Id{ get; set; }
+        public bool Active { get; set; }
+        public string Flag { get; set; }
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+
+            var childContent = await output.GetChildContentAsync();
+
+            //add to context
+            var tabContext = (List<ModalContext>)context.Items[typeof(DauTabsTagHelper)];
+            tabContext.Add(new ModalContext()
+            {
+                TabId =Id,
+                TabContent = childContent,
+                IsDefault=Active,
+                Flag = Flag,
+                DisplayName=Name
+                
+            });
+
+            //generate nothing
+            output.SuppressOutput();
+            //output is suppressed so that we don't have any content rendered
+            output.SuppressOutput();
+           
+        }
+    }
+}
