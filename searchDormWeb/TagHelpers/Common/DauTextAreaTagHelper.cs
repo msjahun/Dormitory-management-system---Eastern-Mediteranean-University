132d6a6dfc760d7cd9ab36a956e8bab77f07f268
Musa Jahun <msjahun@live.com>
2018-09-07 15:26:26 +0300

    Added TextAreaTagHelper

---
+++ searchDormWeb/TagHelpers/Common/DauTextAreaTagHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace searchDormWeb.TagHelpers.Common
+{
+    [HtmlTargetElement("dau-textarea")]
+    public class DauTextAreaTagHelper : TextAreaTagHelper
+    {
+       
+        public DauTextAreaTagHelper(IHtmlGenerator generator) : base(generator)
+        {
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "textarea";
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            output.Attributes.Add("class", "form-control");
+            output.Attributes.Add("id", For.Name + "_id");
+            output.Attributes.Add("rows","10");
+            output.Attributes.Add("cols", "80");
+            //if (For.Metadata.IsRequired && !For.Metadata.IsReadOnly)
+            //    output.PostContent.SetHtmlContent("</div><div><i class=\"required hidden-xs \" style=\"color:red; \">*</i>");
+
+
+           
+        }
+    }
+}
