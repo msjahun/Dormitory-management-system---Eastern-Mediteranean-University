e903092bca02de6ca6e3eafea2a9c121220bcc7f
Musa Jahun <msjahun@live.com>
2018-09-09 00:13:01 +0300

    Added DormitoryBlocks View Model

---
+++ searchDormWeb/Areas/Admin/Models/Catalog/DormitoryBlocksVm.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace searchDormWeb.Areas.Admin.Models.Catalog
+{
+    public class DormitoryBlocksVm
+    {
+    }
+}
