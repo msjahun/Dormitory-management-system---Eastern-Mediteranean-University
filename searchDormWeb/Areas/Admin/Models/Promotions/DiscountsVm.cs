1e829b43a5713b1baaa394ff92bca2b2b5930aa1
Musa Jahun <msjahun@live.com>
2018-09-10 18:14:56 +0300

    added annotation to the viewModel of Discounts

--- searchDormWeb/Areas/Admin/Models/Promotions/DiscountsVm.cs
+++ searchDormWeb/Areas/Admin/Models/Promotions/DiscountsVm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,21 @@ namespace searchDormWeb.Areas.Admin.Models.Promotions
 {
     public class DiscountsVm
     {
-
+        [Display(Name = "Discount name",
+        Description = "Search by discount name."),
+        DataType(DataType.Text),
+        MaxLength(256)]
         public string DiscountName { get; set; }
+
+        [Display(Name = "Coupon code",
+        Description = "Search by discount coupon code."),
+        DataType(DataType.Text),
+        MaxLength(256)]
         public string CouponCode { get; set; }
+
+        [Display(Name = "Discount type",
+        Description = "Search by discount type."),
+        MaxLength(256)]
         public int DiscountType { get; set; }
 
     }
