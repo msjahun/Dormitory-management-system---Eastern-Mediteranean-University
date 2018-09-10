bbbb2d005d54fc334a5b8be5b46c6dffa63fe13a
Musa Jahun <msjahun@live.com>
2018-09-10 18:14:31 +0300

    added annotation to the viewModel of Announcements

--- searchDormWeb/Areas/Admin/Models/Notifications/AnnoucementsVm.cs
+++ searchDormWeb/Areas/Admin/Models/Notifications/AnnoucementsVm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,26 @@ namespace searchDormWeb.Areas.Admin.Models.Notifications
 {
     public class AnnoucementsVm
     {
+        [Display(Name = "Created on",
+        Description = "Filters announcements created on selected date"),
+        DataType(DataType.Date),
+        MaxLength(256)]
+        public DateTime CreatedOn { get; set; }
 
-        public DateTime   CreatedOn { get; set; }
+        [Display(Name = "Priority",
+        Description = "Priority of announcement e.g. Urgent"),
+        MaxLength(256)]
         public int Priority { get; set; }
+
+        [Display(Name = "Start date",
+        Description = "The start date of the search."),
+        DataType(DataType.Date),
+        MaxLength(256)]
         public DateTime StartDate { get; set; }
+
+        [Display(Name = "Active",
+        Description = "Search by a specific status e.g. Active."),
+        MaxLength(256)]
         public int Active { get; set; }
 
     }
