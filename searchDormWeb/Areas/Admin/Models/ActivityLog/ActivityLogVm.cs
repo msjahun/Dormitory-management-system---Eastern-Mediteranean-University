62ff3d4952a36f92f996f6719dc0e5ab2f5f3ee8
Musa Jahun <msjahun@live.com>
2018-09-10 18:11:44 +0300

    added annotation to the viewModel of ActivityLog

--- searchDormWeb/Areas/Admin/Models/ActivityLog/ActivityLogVm.cs
+++ searchDormWeb/Areas/Admin/Models/ActivityLog/ActivityLogVm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,27 @@ namespace searchDormWeb.Areas.Admin.Models.ActivityLog
 {
     public class ActivityLogVm
     {
+        [Display(Name = "Created from",
+        Description = "The creation from the date of the search."),
+        DataType(DataType.Date)]
+        public DateTime CreatedFrom { get; set; }
 
-public DateTime CreatedFrom { get; set; }
-public DateTime CreatedTo { get; set; }
+        [Required]
+        [Display(Name = "Created to",
+        Description = "The creation date of the search."),
+        DataType(DataType.Date),
+        MaxLength(256)]
+        public DateTime CreatedTo { get; set; }
+
+        [Display(Name = "Ip address",
+        Description = "The ip address of the search."),
+        MaxLength(256)]
         public string IpAddress { get; set; }
+
+
+        [Display(Name = "Activity log type",
+        Description = "The activity log type."),
+        MaxLength(256)]
         public int ActivityLogType { get; set; }
 
 
