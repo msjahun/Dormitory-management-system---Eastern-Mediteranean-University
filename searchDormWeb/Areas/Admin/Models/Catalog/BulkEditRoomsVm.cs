72c8a1fe9e1d8f44686eb86eab2cfa782dd55374
Musa Jahun <msjahun@live.com>
2018-09-10 18:12:08 +0300

    added annotation to the viewModel of ActivityLog

--- searchDormWeb/Areas/Admin/Models/Catalog/BulkEditRoomsVm.cs
+++ searchDormWeb/Areas/Admin/Models/Catalog/BulkEditRoomsVm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,17 @@ namespace searchDormWeb.Areas.Admin.Models.Catalog
 {
     public class BulkEditRoomsVm
     {
+        [Display(Name = "Room name",
+        Description = "A room name."),
+        DataType(DataType.Text),
+        MaxLength(256)]
         public string RoomName { get; set; }
+
+
+        [Display(Name = "Dormitory block",
+        Description = "Search by a specific dormitory block."),
+        DataType(DataType.Text),
+        MaxLength(256)]
         public string DormitoryBlock { get; set; }
 
     }
