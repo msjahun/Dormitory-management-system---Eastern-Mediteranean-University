5f9199eac3fdf83224551c8a9a5e571d975dc483
Musa Jahun <msjahun@live.com>
2018-09-10 18:14:44 +0300

    added annotation to the viewModel of Affiliates

--- searchDormWeb/Areas/Admin/Models/Promotions/AffiliatesVm.cs
+++ searchDormWeb/Areas/Admin/Models/Promotions/AffiliatesVm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,28 @@ namespace searchDormWeb.Areas.Admin.Models.Promotions
 {
     public class AffiliatesVm
     {
+
+        [Display(Name = "First name",
+        Description = "Search by a first name."),
+        DataType(DataType.Text),
+        MaxLength(256)]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last name",
+        Description = "Search by a last name."),
+        DataType(DataType.Text),
+        MaxLength(256)]
         public string LastName { get; set; }
+
+        [Display(Name = "Friendly Url name",
+        Description = "Search by a friendly url name."),
+        DataType(DataType.Text),
+        MaxLength(256)]
         public string FriendlyURLName { get; set; }
+
+
+        [Display(Name = "Load only with orders",
+        Description = "Check to load affiliates only with orders placed (by affiliated customers).")]
         public bool LoadOnlyWithOrders { get; set; }
 
 
