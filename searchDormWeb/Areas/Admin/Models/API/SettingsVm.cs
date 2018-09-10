b66cf930e2a3a929b930b1ee8f5ba192b2351897
Musa Jahun <msjahun@live.com>
2018-09-10 18:11:58 +0300

    added annotation to the viewModel of Settings

--- searchDormWeb/Areas/Admin/Models/API/SettingsVm.cs
+++ searchDormWeb/Areas/Admin/Models/API/SettingsVm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,15 @@ namespace searchDormWeb.Areas.Admin.Models.API
     public class SettingsVm
     {
 
+        [Display(Name = "Enable API",
+        Description = "By checking this setting you can Enable/Disable the Web Api"),
+      ]
         public bool EnableAPI { get; set; }
+
+
+        [Display(Name = "Allow requests from swagger",
+        Description = "Swagger is the documentation generation tool used for the API (/Swagger). It has a client that enables it to make GET requests to the API endpoints. By enabling this option you will allow all requests from the swagger client. Do not Enable on a live site, it i only for demo sites or local testing!!!"),
+       ]
         public bool AllowRequestsFromSwagger { get; set; }
 
     }
