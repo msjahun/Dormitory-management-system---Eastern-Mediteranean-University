9995e19e18ce5494bd73163849342fe1540edbe8
Musa Jahun <msjahun@live.com>
2018-09-10 18:14:13 +0300

    added annotation to the viewModel of Dormitories

--- searchDormWeb/Areas/Admin/Models/Configuration/DormitoriesVm.cs
+++ searchDormWeb/Areas/Admin/Models/Configuration/DormitoriesVm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,22 @@ namespace searchDormWeb.Areas.Admin.Models.Configuration
 {
     public class DormitoriesVm
     {
+
+        [Display(Name = "Dormitory name",
+        Description = "A Dormitory name"),
+        DataType(DataType.Text),
+        MaxLength(256)]
         public string  DormitoryName { get; set; }
+
+        [Display(Name = "Dormitory type",
+        Description = "Search by a specific Dormitory Type"),
+        MaxLength(256)]
         public int DormitoryType { get; set; }
+
+        [Display(Name = "Go directly to SKU",
+        Description = "Enter dormitory SKU and click Go"),
+        DataType(DataType.Text),
+        MaxLength(256)]
         public string GoDirectlyToSKU { get; set; }
 
 
