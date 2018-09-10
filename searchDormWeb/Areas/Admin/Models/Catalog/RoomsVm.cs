f63be3b59e2875a9ea1d58ee01085b2dc1b86963
Musa Jahun <msjahun@live.com>
2018-09-10 18:13:01 +0300

    added annotation to the viewModel of Rooms

--- searchDormWeb/Areas/Admin/Models/Catalog/RoomsVm.cs
+++ searchDormWeb/Areas/Admin/Models/Catalog/RoomsVm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,27 @@ namespace searchDormWeb.Areas.Admin.Models.Catalog
 {
     public class RoomsVm
     {
-            public string RoomName { get; set; }
-            public int DormitoryBlock { get; set; }
-            public int Published { get; set; }
-            public string GoDirectoryToRoomSKU { get; set; }
+        [Display(Name = "Room name",
+        Description = "A room name."),
+        DataType(DataType.Text),
+        MaxLength(256)]
+        public string RoomName { get; set; }
+
+        [Display(Name = "Dormitory block",
+        Description = "Search by a specific Dormitory Block"),
+        MaxLength(256)]
+        public int DormitoryBlock { get; set; }
+
+        [Display(Name = "Published",
+        Description = "Search by a \"published\" property"),
+        MaxLength(256)]
+        public int Published { get; set; }
+
+        [Display(Name = "Go directly to Room SKU",
+        Description = "Enter room SKU and click Go"),
+        DataType(DataType.Text),
+        MaxLength(256)]
+        public string GoDirectoryToRoomSKU { get; set; }
 
     }
 }
