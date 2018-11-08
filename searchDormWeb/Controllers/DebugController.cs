2b38fc473530ef94056fdbf61ec4ee2ec215cc93
Musa Jahun <msjahun@live.com>
2018-11-08 03:45:38 +0200

    added the debug actions in the debug controller and made explore emu the default route for debug

--- searchDormWeb/Controllers/DebugController.cs
+++ searchDormWeb/Controllers/DebugController.cs
@@ -8,9 +8,96 @@ namespace searchDormWeb.Controllers
 {
     public class DebugController : Controller
     {
-        public IActionResult Index()
+        public IActionResult AccountProfile()
         {
             return View();
         }
+
+        public IActionResult AccountSettings()
+        {
+            return View();
+        }
+
+        public IActionResult AccountBilling()
+        {
+            return View();
+        }
+
+        public IActionResult AccountNotifications()
+        {
+            return View();
+        }
+
+        public IActionResult BookingCart()
+        {
+            return View();
+        }
+
+        public IActionResult BookingCheckoutCustomer()
+        {
+            return View();
+        }
+
+        public IActionResult BookingCheckoutAddress()
+        {
+            return View();
+        }
+
+        public IActionResult BookingCheckoutPayment()
+        {
+            return View();
+        }
+
+        public IActionResult BookingCartEmpty()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult RecoverAccount()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult FrequentlyAskedQuestions()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult ExploreEMU()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult SearchDormMobileApp()
+        {
+            return View();
+        }
+
+
+
+
+
     }
 }
