60963f09a061a762ab6d9faaa3032b00d94968e9
Musa Jahun <msjahun@live.com>
2018-11-09 02:12:41 +0200

    created the ExploreEMUpic api for suplying explore emu page with json picture urls

--- searchDormWeb/Controllers/DebugController.cs
+++ searchDormWeb/Controllers/DebugController.cs
@@ -93,6 +93,59 @@ namespace searchDormWeb.Controllers
             return View();
         }
 
+        
+
+        public IActionResult ExploreEMUPicsApi()
+        {
+
+           
+            
+
+            return Json(new List<ExploreImages> {
+            new ExploreImages{
+                ImageUrl = "https://dormitories.emu.edu.tr/PhotoGalleries/dormitories/nural/photo6010428924410047910.jpg?RenditionID=9",
+                DormitoryName = "Alfam Dormitory6",
+                DormitoryDescription = "Wonderful dormitory"},
+
+
+                new ExploreImages { ImageUrl = "https://dormitories.emu.edu.tr/PhotoGalleries/dormitories/nural/photo6010428924410047915.jpg?RenditionID=5",
+                DormitoryName = "Alfam Dormitory5",
+                DormitoryDescription = "Wonderful dormitory"},
+
+
+              new ExploreImages   { ImageUrl = "https://dormitories.emu.edu.tr/PhotoGalleries/dormitories/nural/IMG_0050%20(1).jpg?RenditionID=8",
+                DormitoryName = "Alfam Dormitory6",
+                DormitoryDescription = "Wonderful dormitory"},
+
+                      new ExploreImages   { ImageUrl = "https://dormitories.emu.edu.tr/PhotoGalleries/dormitories/nural/IMG_0027%20(1).jpg?RenditionID=7",
+                DormitoryName = "Alfam Dormitory7",
+                DormitoryDescription = "Wonderful dormitory"},
+
+
+                              new ExploreImages   { ImageUrl = "https://dormitories.emu.edu.tr/PhotoGalleries/dormitories/nural/IMG_0004.jpg?RenditionID=8",
+                DormitoryName = "Alfam Dormitory8",
+                DormitoryDescription = "Wonderful dormitory"},
+
+                              new ExploreImages   { ImageUrl = "https://dormitories.emu.edu.tr/PhotoGalleries/dormitories/astra/IMG-20171014-WA0010.jpg?RenditionID=5",
+                DormitoryName = "Alfam Dormitory5",
+                DormitoryDescription = "Wonderful dormitory"},
+
+                              new ExploreImages   { ImageUrl = "https://dormitories.emu.edu.tr/PhotoGalleries/dormitories/2017/kamacioglu/S%C3%9C%C4%B0T_1.jpg?RenditionID=9",
+                DormitoryName = "Alfam Dormitory6",
+                DormitoryDescription = "Wonderful dormitory"},
+
+                              new ExploreImages   { ImageUrl = "https://dormitories.emu.edu.tr/PhotoGalleries/dormitories/popart/TV%20ROOM%20KING%20EXCLUSIVE.jpg?RenditionID=8",
+                DormitoryName = "Alfam Dormitory8",
+                DormitoryDescription = "Wonderful dormitory"},
+
+
+            }
+
+
+                );
+            
+            }
+
 
 
         public IActionResult SearchDormMobileApp()
@@ -104,5 +157,13 @@ namespace searchDormWeb.Controllers
 
 
 
+    }
+
+
+    public class ExploreImages
+    {
+        public string ImageUrl { get; set; }
+        public string DormitoryName { get; set; }
+        public string DormitoryDescription { get; set; }
     }
 }
