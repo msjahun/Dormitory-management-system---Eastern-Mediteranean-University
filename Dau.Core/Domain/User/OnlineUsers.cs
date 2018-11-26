47feb064aa14d1f6b34b43fd7748fdd3d46f9e3f
Musa Jahun <msjahun@live.com>
2018-11-26 19:31:12 +0200

    Added OnlineUser class and OnlineUserMap

---
+++ Dau.Core/Domain/User/OnlineUsers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dau.Core.Domain.User
+{
+   public class OnlineUsers
+    {
+        public int Id { get; set; }
+        public string UserInfo { get; set; }
+        public string IpAddress { get; set; }
+        public string Location { get; set;  }
+        public string LastActivity { get; set; }
+
+        public string LastVisitedPage { get; set; }
+        public DateTime LastActivityTime { get; set; }
+    }
+}
---
+++ Dau.Data/Mapping/User/OnlineUsersMap.cs
@@ -0,0 +1,60 @@
+﻿using Dau.Core.Domain.User;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dau.Data.Mapping.User
+{
+    public class OnlineUsersMap : IEntityTypeConfiguration<OnlineUsers>
+    {
+
+
+        public void Configure(EntityTypeBuilder<OnlineUsers> builder)
+        {
+            builder.ToTable("OnlineUsers");
+
+
+            builder.Property(e => e.Id).HasColumnName("ID");
+
+
+            builder.Property(e => e.UserInfo)
+                .HasColumnName("UserInfo")
+                .HasMaxLength(400)
+             .IsUnicode(false); 
+
+
+            builder.Property(e => e.IpAddress)
+                .HasColumnName("IpAddress")
+                .HasMaxLength(400)
+             .IsUnicode(false); 
+
+            builder.Property(e => e.Location)
+             .IsRequired()
+             .HasColumnName("Location")
+             .HasMaxLength(400)
+             .IsUnicode(false);
+
+            builder.Property(e => e.LastActivity)
+              .IsRequired()
+              .HasColumnName("LastActivity")
+              .HasMaxLength(400)
+              .IsUnicode(false);
+
+            builder.Property(e => e.LastVisitedPage)
+         .IsRequired()
+         .HasColumnName("LastVisitedPage")
+         .HasMaxLength(400)
+         .IsUnicode(false);
+
+            builder.Property(e => e.LastActivityTime)
+                 .HasColumnName("LastActivityTime")
+                 .HasColumnType("datetime2");
+
+        }
+
+
+
+    }
+}
