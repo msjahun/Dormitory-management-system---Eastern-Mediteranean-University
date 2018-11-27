f54840a5f65145c7c82ffc5c4d14585285e5ce39
Musa Jahun <msjahun@live.com>
2018-11-27 16:08:38 +0200

    Added IBaseSeo in which all the classes with relationship with SEO will implement and also Added SEO.cs

---
+++ Dau.Core/Domain/SearchEngineOptimization/IBaseSeo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dau.Core.Domain.SearchEngineOptimization
+{
+  public  interface IBaseSeo
+    {
+        int SeoId { get; set; }
+         Seo Seo{ get; set; }
+    }
+}
---
+++ Dau.Core/Domain/SearchEngineOptimization/Seo.cs
@@ -0,0 +1,35 @@
+﻿using Dau.Core.Domain.Catalog;
+using Dau.Core.Domain.ContentManagement;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dau.Core.Domain.SearchEngineOptimization
+{
+   public class Seo
+    {
+        public Seo()
+        {
+            DormitoryBlocks = new HashSet<DormitoryBlock>();
+            Topics = new HashSet<Topic>();
+
+        }
+
+        public int Id { get; set; }
+        public string MetaKeywords { get; set; }
+
+
+        public string MetaDescription { get; set; }
+
+
+        public string MetaTitle { get; set; }
+
+
+        public string SearchEngineFriendlyPageName { get; set; }
+
+        public ICollection<DormitoryBlock> DormitoryBlocks{ get; set; }
+        public ICollection<Topic> Topics{ get; set; }
+
+       
+    }
+}
