2d552a083ce803095e8a6e5dbbd3e033edbe379f
Musa Jahun <msjahun@live.com>
2018-11-27 16:47:23 +0200

    changed Address folder to Addresses

---
+++ Dau.Core/Domain/Addresses/Address.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dau.Core.Domain.Reservations;
+namespace Dau.Core.Domain.Addresses
+{
+   public class Address
+    {
+        public Address()
+        {
+            Reservations = new HashSet<Reservation>();
+        }
+
+        //public string FullName { get; set; }
+        //public string Email { get; set; }
+        //public string PhoneNumber { get; set; }
+
+        //Do we needs things above ^ since there will be in the user table, not atomic
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string StateProvince { get; set; }
+        public string ZipPostalCode { get; set; }
+        public string Country { get; set; }
+
+        //a lot of the entities will use address
+
+        public ICollection<Reservation> Reservations{ get; set; }
+    }
+}
