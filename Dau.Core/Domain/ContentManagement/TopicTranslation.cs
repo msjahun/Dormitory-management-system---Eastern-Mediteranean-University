777f2e251c7bef12c04099177581deb1e239fd4c
Musa Jahun <msjahun@live.com>
2018-11-27 14:55:55 +0200

    TopicTranslation now inherits from base language class

--- Dau.Core/Domain/ContentManagement/TopicTranslation.cs
+++ Dau.Core/Domain/ContentManagement/TopicTranslation.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Dau.Core.Domain.Language;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Dau.Core.Domain.ContentManagement
 {
-   public class TopicTranslation
+   public class TopicTranslation : BaseLanguage
+
+
     {
+
+        public int TopicNonTransId { get; set; }
         public string Title { get; set; }
 
         public string Body { get; set; }
