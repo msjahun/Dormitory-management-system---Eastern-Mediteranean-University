aafd0b71c969a5e54b2ea0c0bb2652ac854d0dcd
Musa Jahun <msjahun@live.com>
2018-11-30 13:15:20 +0200

    added the tables and also their corresponding configurations to the Fees_and_facilitiesContext

--- Dau.Data/fees_and_facilitiesContext.cs
+++ Dau.Data/fees_and_facilitiesContext.cs
@@ -16,18 +16,45 @@ using Dau.Data.Mapping.Room;
 using Dau.Core.Domain.Logging;
 using Dau.Data.Mapping.Logging;
 using Dau.Data.Mapping.User;
+using Microsoft.Extensions.Configuration;
+using Dau.Core.Domain.Activity;
+using Dau.Core.Domain.Addresses;
+using Dau.Core.Domain.API;
+using Dau.Core.Domain.Catalog;
+using Dau.Core.Domain.ContentManagement;
+using Dau.Core.Domain.CountryInformation;
+using Dau.Core.Domain.CurrencyInformation;
+using Dau.Core.Domain.EmailAccountInformation;
+using Dau.Core.Domain.MobileAppManager;
+using Dau.Core.Domain.Notifications;
+using Dau.Core.Domain.Promotions;
+using Dau.Core.Domain.Reservations;
+using Dau.Core.Domain.SearchEngineOptimization;
+using Dau.Core.Domain.System;
+using Dau.Data.Mapping.Activity;
+using Dau.Data.Mapping.Addresses;
+using Dau.Data.Mapping.API;
+using Dau.Data.Mapping.Catalog;
+using Dau.Data.Mapping.ContentManagement;
+using Dau.Data.Mapping.CountryInformation;
+using Dau.Data.Mapping.CurrencyInformation;
+using Dau.Data.Mapping.EmailAccountInformation;
+using Dau.Data.Mapping.MobileAppManager;
+using Dau.Data.Mapping.Notifications;
+using Dau.Data.Mapping.Promotions;
+using Dau.Data.Mapping.Reservations;
+using Dau.Data.Mapping.SearchEngineOptimization;
+using Dau.Data.Mapping.System;
 
 namespace Dau.Data
 {
     public partial class Fees_and_facilitiesContext : IdentityDbContext<User, UserRole,string>
     {
-      
-
         public Fees_and_facilitiesContext()
-           
         {
         }
 
+      
         public virtual DbSet<AccountInformationParameter> AccountInformationParameter { get; set; }
         public virtual DbSet<AccountInformationParameterTranslation> AccountInformationParameterTranslation { get; set; }
         public virtual DbSet<AccountParameterValues> AccountParameterValues { get; set; }
@@ -54,13 +81,48 @@ namespace Dau.Data
         //new tables
         public virtual DbSet<Log> Log{ get; set; }
         public virtual DbSet<OnlineUsers> OnlineUsers{ get; set; }
+        public virtual DbSet<ActivityLog> ActivityLog{ get; set; }
+        public virtual DbSet<Address> Address{ get; set; }
+        public virtual DbSet<APIClient> APIClient{ get; set; }
+        public virtual DbSet<APISettings> APISettings{ get; set; }
+        public virtual DbSet<DormitoryBlock> DormitoryBlock { get; set; }
+        public virtual DbSet<DormitoryBlockTranslation> DormitoryBlockTranslation { get; set; }
+        public virtual DbSet<Review> Review { get; set; }
+        public virtual DbSet<MessageTemplate> MessageTemplate { get; set; }
+        public virtual DbSet<MessageTemplateTranslation> MessageTemplateTranslation { get; set; }
+        public virtual DbSet<Poll> Poll { get; set; }
+        public virtual DbSet<PollAnswers> PollAnswers { get; set; }
+        public virtual DbSet<Survey> Survey { get; set; }
+        public virtual DbSet<SurveyQuestionsAndAnswers> SurveyQuestionsAndAnswers { get; set; }
+        public virtual DbSet<Topic> Topic { get; set; }
+        public virtual DbSet<TopicTranslation> TopicTranslation { get; set; }
+        public virtual DbSet<Country> Country { get; set; }
+        public virtual DbSet<CountryTranslation> CountryTranslation { get; set; }
+        public virtual DbSet<StateAndProvince> StateAndProvince { get; set; }
+        public virtual DbSet<Currency> Currency { get; set; }
+        public virtual DbSet<CurrencyTranslation> CurrencyTranslation { get; set; }
+        public virtual DbSet<EmailAccount> EmailAccount { get; set; }
+        public virtual DbSet<PushNotification> PushNotification { get; set; }
+        public virtual DbSet<Announcement> Announcement { get; set; }
+        public virtual DbSet<Notification> Notification { get; set; }
+        public virtual DbSet<Affiliate> Affiliate { get; set; }
+        public virtual DbSet<Campaign> Campaign { get; set; }
+        public virtual DbSet<Discount> Discount { get; set; }
+        public virtual DbSet<NewsLetterSubscribers> NewsLetterSubscribers { get; set; }
+        public virtual DbSet<CancelReservationRequests> CancelReservationRequests { get; set; }
+        public virtual DbSet<OrderNotes> OrderNotes { get; set; }
+        public virtual DbSet<Reservation> Reservation { get; set; }
+        public virtual DbSet<RoomReservation> RoomReservation { get; set; }
+        public virtual DbSet<Seo> Seo { get; set; }
+        public virtual DbSet<MessageQueue> MessageQueue { get; set; }
+
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
 
 
-             optionsBuilder.UseSqlServer("Data Source=DARK-SHILLA\\SQLEXPRESS;Initial Catalog=fees_and_facilities;Integrated Security=True");
+        optionsBuilder.UseSqlServer("Data Source=DARK-SHILLA\\SQLEXPRESS;Initial Catalog=fees_and_facilities;Integrated Security=True");
             //  optionsBuilder.UseSqlServer("data source=193.140.173.173;initial catalog=SearchDormDb;user id=SearchDormUsr;password=ergec.senturk@emu.edu.tr;multipleactiveresultsets=True;");
             //optionsBuilder.UseSqlServer("data source=sql6006.site4now.net;initial catalog=DB_A2B24A_testing;user id=DB_A2B24A_testing_admin;password=Mami1961;multipleactiveresultsets=True;");
            // optionsBuilder.UseSqlServer("Data Source=SQL-SERVER-INST;Initial Catalog=fees_and_facilities_prod;Integrated Security=True");
@@ -93,10 +155,52 @@ namespace Dau.Data
             modelBuilder.ApplyConfiguration(new RoomTableTranslationMap());
 
 
-            //new tables config
+            
             modelBuilder.ApplyConfiguration(new LogMap());
             modelBuilder.ApplyConfiguration(new OnlineUsersMap());
-         
+
+
+
+            modelBuilder.ApplyConfiguration(new ActivityLogMap());
+            modelBuilder.ApplyConfiguration(new AddressMap());
+            modelBuilder.ApplyConfiguration(new APIClientMap());
+            modelBuilder.ApplyConfiguration(new APISettingsMap());
+            modelBuilder.ApplyConfiguration(new DormitoryBlockMap());
+            modelBuilder.ApplyConfiguration(new DormitoryBlockTranslationMap());
+            modelBuilder.ApplyConfiguration(new ReviewMap());
+            modelBuilder.ApplyConfiguration(new MessageTemplateMap());
+            modelBuilder.ApplyConfiguration(new MessageTemplateTranslationMap());
+            modelBuilder.ApplyConfiguration(new PollMap());
+            modelBuilder.ApplyConfiguration(new PollAnswersMap());
+            modelBuilder.ApplyConfiguration(new SurveyMap());
+            modelBuilder.ApplyConfiguration(new SurveyQuestionsAndAnswersMap());
+            modelBuilder.ApplyConfiguration(new TopicMap());
+            modelBuilder.ApplyConfiguration(new TopicTranslationMap());
+            modelBuilder.ApplyConfiguration(new CountryMap());
+            modelBuilder.ApplyConfiguration(new CountryTranslationMap());
+            modelBuilder.ApplyConfiguration(new StateAndProvinceMap());
+            modelBuilder.ApplyConfiguration(new CurrencyMap());
+            modelBuilder.ApplyConfiguration(new CurrencyTranslationMap());
+            modelBuilder.ApplyConfiguration(new EmailAccountMap());
+            modelBuilder.ApplyConfiguration(new PushNotificationMap());
+            modelBuilder.ApplyConfiguration(new AnnouncementsMap());
+            modelBuilder.ApplyConfiguration(new NotificationMap());
+            modelBuilder.ApplyConfiguration(new AffiliateMap());
+            modelBuilder.ApplyConfiguration(new CampaignMap());
+            modelBuilder.ApplyConfiguration(new DiscountMap());
+            modelBuilder.ApplyConfiguration(new NewsletterSubscribersMap());
+            modelBuilder.ApplyConfiguration(new CancelReservationRequestMap());
+            modelBuilder.ApplyConfiguration(new OrderNotesMap());
+            modelBuilder.ApplyConfiguration(new ReservationMap());
+            modelBuilder.ApplyConfiguration(new RoomReservationMap());
+            modelBuilder.ApplyConfiguration(new SeoMap());
+            modelBuilder.ApplyConfiguration(new MessageQueueMap());
+
+
+
+
+
+            //new tables config
         }
     }
 }
