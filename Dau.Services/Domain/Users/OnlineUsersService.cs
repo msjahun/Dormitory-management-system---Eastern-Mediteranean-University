ac0dbe582dbc03be3e879f33a1bc56fea727419e
Musa Jahun <msjahun@live.com>
2018-12-02 14:17:56 +0200

    OnlineUsersService now use repoistory pattern

--- Dau.Services/Domain/Users/OnlineUsersService.cs
+++ Dau.Services/Domain/Users/OnlineUsersService.cs
@@ -4,23 +4,30 @@ using System.Collections.Generic;
 using System.Text;
 using Dau.Core.Domain.User;
 using System.Linq;
+using Dau.Data.Repository;
 
 namespace Dau.Services.Domain.Users
 {
    public class OnlineUsersService : IOnlineUsersService
     {
-        private Fees_and_facilitiesContext _context = new Fees_and_facilitiesContext();
+        private readonly IRepository<OnlineUsers> _onlineUsersRepository;
+
+        public OnlineUsersService(IRepository<OnlineUsers> OnlineUsersRepository)
+        {
+            _onlineUsersRepository = OnlineUsersRepository;
+        }
 
         public List<OnlineUsers> GetOnlineUsers()
         {
             var OnlineUsers = new List<OnlineUsers>();
-           return _context.OnlineUsers.OrderByDescending(d => d.LastActivityTime).Where(t => t.LastActivityTime >= DateTime.Now).ToList();
+            
+            return _onlineUsersRepository.List().OrderByDescending(d => d.LastActivityTime).Where(t => t.LastActivityTime >= DateTime.Now).ToList();
         }
 
 
-        public async void UpdateOnlineUserAsync(OnlineUsers user)
+        public  void UpdateOnlineUserAsync(OnlineUsers user)
         {
-            var userInDb = _context.OnlineUsers.Where(d => d.UserInfo == user.UserInfo && d.IpAddress == user.IpAddress).FirstOrDefault();
+            var userInDb = _onlineUsersRepository.List().Where(d => d.UserInfo == user.UserInfo && d.IpAddress == user.IpAddress).FirstOrDefault();
             if (userInDb != null)
             {
 
@@ -28,14 +35,14 @@ namespace Dau.Services.Domain.Users
                 //check if user already exist on the table,
                 userInDb.LastActivityTime = DateTime.Now.AddMinutes(1);
                 userInDb.LastVisitedPage = user.LastVisitedPage;
-                _context.OnlineUsers.Update(userInDb);
-                await _context.SaveChangesAsync();
+                _onlineUsersRepository.Update(userInDb);
+               
             }
             else {
                 //if user doesn't exist add user to the table
                 user.LastActivityTime= DateTime.Now.AddMinutes(1);
-                _context.OnlineUsers.Add(user);
-                await _context.SaveChangesAsync();
+                 _onlineUsersRepository.Insert(user);
+             
                 //if user exist update time, use ip address and userInfo for checking
             }
         }
