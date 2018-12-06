21600d5112f6d8bea4df2f8d7c74c77aec1e82de
Musa Jahun <msjahun@live.com>
2018-12-06 02:29:57 +0200

    Added display name then localized them for the LoginViewModel

--- searchDormWeb/Models/LoginViewModel.cs
+++ searchDormWeb/Models/LoginViewModel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿  using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -10,13 +10,16 @@ namespace searchDormWeb.Models
     {
         [Required]
         [EmailAddress]
+        [Display(Name = "Email")]
         public string Email { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string Password { get; set; }
 
         [Display(Name ="Remember Me")]
+       
         public bool RememberMe { get; set; }
 
         public string ReturnUrl { get; set; }
