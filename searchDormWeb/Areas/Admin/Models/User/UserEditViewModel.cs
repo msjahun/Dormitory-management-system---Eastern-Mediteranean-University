06c241866664acf6326d69a484548a4ea252fcf5
Musa Jahun <msjahun@live.com>
2018-12-06 02:28:39 +0200

    changed Id to long from int and also removed an almost injected DormitoryService

--- searchDormWeb/Areas/Admin/Models/User/UserEditViewModel.cs
+++ searchDormWeb/Areas/Admin/Models/User/UserEditViewModel.cs
@@ -10,7 +10,7 @@ namespace searchDormWeb.Areas.Admin.Models.User
 {
     public class UserEditViewModel
     {
-        private DormitoryService _dormitoryService;
+  
 
      
 
@@ -27,7 +27,7 @@ namespace searchDormWeb.Areas.Admin.Models.User
         public List<SelectListItem> CustomerRoles { get; set; }
 
         [Display(Name = "Manager of dormitory")]
-        public int ManagerOfDormitory { get; set; }
+        public long ManagerOfDormitory { get; set; }
 
         public List<SelectListItem> Dormitories { get; set; }
 
