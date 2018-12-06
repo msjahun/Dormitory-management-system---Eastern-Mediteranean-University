754877a9be598641f72ccc9a84db9825e3a3a73e
Musa Jahun <msjahun@live.com>
2018-12-06 02:26:57 +0200

    tried to seed data into some tables but failed miserably

--- Dau.Data/Extensions/ModelBuilderExtensions.cs
+++ Dau.Data/Extensions/ModelBuilderExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Dau.Core.Domain.User;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,8 +9,38 @@ namespace Dau.Data.Extensions
 {
     public static class ModelBuilderExtensions
     {
+        private static RoleManager<UserRole> _roleManager;
+
+        //public static void Init(RoleManager<UserRole> roleManager)
+        //{
+        //    _roleManager = roleManager;
+        //}
+
         public static void Seed(this ModelBuilder modelBuilder)
         {
+           
+
+            ////create user role and redirect to edit_userRole page
+            //var role = new UserRole();
+            //role.Name = "Registered";
+            //role.Access = "[{\"Id\":\":\",\"Name\":\"\",\"DisplayName\":null,\"AreaName\":\"\",\"Actions\":[]}]";
+            //role.IsActive = true;
+            //role.IsSystemRole = true;
+            //_roleManager.CreateAsync(role);
+
+            //role = new UserRole();
+            //role.Name = "ADMINISTRATOR";
+            //role.Access = "[{\"Id\":\":\",\"Name\":\"\",\"DisplayName\":null,\"AreaName\":\"\",\"Actions\":[]}]";
+            //role.IsActive = true;
+            //role.IsSystemRole = true;
+            //_roleManager.CreateAsync(role);
+
+            // role = new UserRole();
+            //role.Name = "DORMITORY ADMINISTRATOR";
+            //role.Access = "[{\"Id\":\":\",\"Name\":\"\",\"DisplayName\":null,\"AreaName\":\"\",\"Actions\":[]}]";
+            //role.IsActive = true;
+            //role.IsSystemRole = true;
+            //_roleManager.CreateAsync(role);
             //put all your seed data here,
 
             //One user name and password for login
@@ -26,6 +58,9 @@ namespace Dau.Data.Extensions
             //    new Book { BookId = 2, AuthorId = 1, Title = "King Lear" },
             //    new Book { BookId = 3, AuthorId = 1, Title = "Othello" }
             //);
+
+
+
         }
     }
 }
