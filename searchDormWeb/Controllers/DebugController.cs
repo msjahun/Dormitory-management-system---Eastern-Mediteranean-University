cedb1d080d5185728d1ea5ea3b9e46f017cc433c
mjahun <mjahun@SQL-SERVER-INST>
2018-12-08 17:21:16 +0000

    I added a new action in the process of wanting to add culture to the cookies

--- searchDormWeb/Controllers/DebugController.cs
+++ searchDormWeb/Controllers/DebugController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace searchDormWeb.Controllers
@@ -18,8 +20,18 @@ namespace searchDormWeb.Controllers
             return View();
         }
 
+
+      
+
         public IActionResult DormitoryDetail()
         {
+            string culture = "tr-TR";
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+            );
+
             return View();
         }
 
