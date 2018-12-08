754d486b346a7fa0411f2bf9e10ba2a20a45c1a2
Musa Jahun <msjahun@live.com>
2018-12-08 15:31:07 +0200

    Started moving Localization to Dau.services

---
+++ Dau.Services/Localization/Components/LanguageSelector/Models/LanguageSelectorModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dau.Services.Localization.Components.LanguageSelector.Models
+{
+    class LanguageSelectorModel
+    {
+    }
+}
