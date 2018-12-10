5c67ebe39db1f02847f9351f4e5145caa60bbb71
Musa Jahun <msjahun@live.com>
2018-12-10 21:23:54 +0200

    Added new Dormitory views that will be used to refactor DormitoryDetail view

--- searchDormWeb/Controllers/DormitoryController.cs
+++ searchDormWeb/Controllers/DormitoryController.cs
@@ -15,5 +15,60 @@ namespace searchDormWeb.Controllers
 
             return View("DormitoryDetail");
         }
+
+
+        public IActionResult GetAreaInfoSection()
+        {return PartialView("_AreaInfoSection");
+        }
+
+
+        public IActionResult GetCommentsSection()
+        {
+            return PartialView("_CommentsSection");
+        }
+
+        public IActionResult GetDormitoryDescriptionSection()
+        {
+            return PartialView("_DormitoryDescriptionSection");
+        }
+
+
+        public IActionResult GetFacilitiesSection()
+        {
+            return PartialView("_FacilitiesSection");
+        }
+           public IActionResult GetFilterSection()
+        {
+            return PartialView("_FilterSection");
+        }
+
+
+        public IActionResult GetGoodToKnowSection()
+        {
+            return PartialView("_GoodToKnowSection");
+        }
+
+
+        public IActionResult GetReviewBottomSection()
+        {
+            return PartialView("_ReviewBottomSection");
+        }
+
+
+        public IActionResult GetRoomSection()
+        {
+            return PartialView("_RoomSection");
+        }
+
+
+        public IActionResult GetSlidersSection()
+        {
+            return PartialView("_SlidersSection");
+        }
+
+        public IActionResult GetTopnavDormitorySection()
+        {
+            return PartialView("_TopnavDormitorySection");
+        }
     }
 }
