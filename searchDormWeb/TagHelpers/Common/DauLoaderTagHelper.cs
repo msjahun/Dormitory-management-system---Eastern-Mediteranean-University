a891215b42b4f928459c3509f06860b958334c48
Musa Jahun <msjahun@live.com>
2018-12-11 17:33:25 +0200

    Created two Taghelpers one for Loader and the other For Rating

---
+++ searchDormWeb/TagHelpers/Common/DauLoaderTagHelper.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace searchDormWeb.TagHelpers.Common
+{
+    [HtmlTargetElement("dau-loader")]
+    public class DauLoaderTagHelper:TagHelper
+    {
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            //tag details
+            output.TagName = "div";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Content.Clear();
+
+
+
+          
+
+            for (int i = 0; i < 4; i++)
+            {
+                
+                output.PostContent.AppendHtml("<div></div>");
+            }
+
+            output.PreElement.SetHtmlContent("<div class=\"text-center mt-5\">");
+            output.PostElement.SetHtmlContent("</div>");
+
+
+
+
+
+            output.Attributes.Add("class", "lds-ring");
+
+
+        }}
+    }
---
+++ searchDormWeb/TagHelpers/Common/DauRatingTagHelper.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace searchDormWeb.TagHelpers.Common
+{
+    [HtmlTargetElement("dau-rating")]
+    public class DauRatingTagHelper : TagHelper
+    {
+        //<dau-label></dau-label>
+       public string Rating {get; set;}
+
+
+
+      
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            //tag details
+            output.TagName = "span";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Content.Clear();
+
+
+
+            var RatingNo = Convert.ToDouble(Rating);
+                
+                for (int i = 0; i < 10; i += 2)
+                {var star = new TagBuilder("i");
+                    if (i < Math.Floor(RatingNo))
+                    {
+                    star.Attributes.Add("class", "star fas fa-star voted");
+
+                    }
+                    else
+                    {
+                    star.Attributes.Add("class", "star fas fa-star ");
+                    }
+                output.PostContent.AppendHtml(" ");
+                output.PostContent.AppendHtml(star);
+                }
+
+               
+              
+            
+
+
+
+            output.Attributes.Add("class", "static-rating static-rating-sm");
+       
+
+
+        }
+    }
+}
