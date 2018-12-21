bf934b7ab5d60d3bc997515e9bfb85a651fba956
Musa Jahun <msjahun@live.com>
2018-12-21 02:05:14 +0200

    Added responseResult for some api's

---
+++ searchDormWeb/Models/ResponseResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace searchDormWeb.Models
+{
+    public class ResponseResult
+    {
+       
+            public bool Response { get; set; }
+            public string StatusCode { get; set; }
+        
+    }
+}
