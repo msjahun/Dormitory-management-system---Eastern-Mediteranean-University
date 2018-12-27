2b6d26601e39dbb532b30d19592fe76a44438295
Musa Jahun <msjahun@live.com>
2018-12-27 14:32:41 +0200

    Created MobileApiService, I'll move all the api services here

---
+++ Dau.Services/Domain/MobileApiServices/MobileApiService.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dau.Services.Domain.MobileApiServices
+{
+   public class MobileApiService
+    {
+
+        public void BookingByBookingIdService()
+        {
+
+        }
+
+        public void CancelBookingService()
+        {
+
+        }
+
+        public void CreateBookingService()
+        {
+
+
+        }
+
+
+        public void EditBookingService()
+        {
+
+
+        }
+
+
+        public void FacilitiesFilterService()
+        {
+
+        }
+
+
+        public void GetBookingService()
+        {
+
+
+        }
+
+
+        public void GetBookingsByCustomerIdService()
+        {
+
+
+        }
+
+        public void GetCurrencyService()
+        {
+
+        }
+
+        public void GetDormitoriesService()
+        {
+
+        }
+
+
+        public void GetDormitoryDetailByIdService()
+        {
+
+        }
+
+        public void GetHighlyRatedDormitoriesService()
+        {
+
+        }
+
+
+        public void GetMostPopularDormitoriesService()
+        {
+
+
+        }
+
+
+        public void GetRoomByDormitoryIdService()
+        {
+
+        }
+
+        public void GetRoomByIdService()
+        {
+
+        }
+
+
+        public void GetSearchDormitoriesByFilterService()
+        {
+
+        }
+
+        public void GiveAppFeedBackService()
+        {
+
+
+        }
+
+        public void PaymentConfirmationService()
+        {
+
+        }
+
+
+        public void RateYourStayService()
+        {
+
+        }
+    }
+}
