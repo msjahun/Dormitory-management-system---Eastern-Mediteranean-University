d82b278e497035f38e81d86646b5db06ba476cc6
Musa Jahun <msjahun@live.com>
2018-12-28 21:31:58 +0200

    Added GetDormitoriesFilter and GetDormitoryTypes filter to the search controller and also added Options.cshtml and made some changes to the FilterBottomFacilities and Filtertop cshtml

--- searchDormWeb/Controllers/SearchController.cs
+++ searchDormWeb/Controllers/SearchController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Dau.Services.Domain.SearchResultService;
 using Dau.Services.Domain.OnScrollAlertService;
+using Dau.Services.Domain.FeaturesServices;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,20 +14,25 @@ namespace searchDormWeb.Controllers
     public class SearchController : Controller
     {
         private readonly IOnScrollAlertService _getOnScrollAlertService;
+        private readonly IFeaturesService _featuresService;
         private readonly IRoomResultService _roomResultService;
         private readonly IFilterBottomService _filterBottomService;
         private readonly IDormitoryResultService _dormitoryResultService;
+        private readonly ISearchService _searchService;
 
         public SearchController(IOnScrollAlertService getOnScrollAlertService,
             IRoomResultService roomResultService,
             IFilterBottomService filterBottomService,
-            IDormitoryResultService dormitoryResultService )
+            IDormitoryResultService dormitoryResultService,
+            IFeaturesService featuresService,
+            ISearchService searchService)
         {
             _getOnScrollAlertService = getOnScrollAlertService;
-
+            _featuresService = featuresService;
             _roomResultService=  roomResultService;
             _filterBottomService= filterBottomService;
             _dormitoryResultService= dormitoryResultService;
+            _searchService=searchService;
         }
 
         public IActionResult Results()
@@ -51,14 +57,27 @@ namespace searchDormWeb.Controllers
         }
 
 
-        public IActionResult GetRoomResultView()
+        public IActionResult GetRoomResultView(GetRoomResultViewModel InputModel)
         {
-           var modelList = _roomResultService.GetRoomResult();
-        
+            //if filters are received well, remove the class and send it to the GetRoomResult service and use it for filtering
+           var modelList = _roomResultService.GetRoomResult(InputModel);
+            var featuresLogSuccess = _featuresService.UpdateFeaturesHitCount(InputModel.FeaturesIds);
 
             return PartialView("_RoomResultView", modelList);
         }
 
+
+        public IActionResult GetDormitoryTypesFilter()
+        {
+            var model = _searchService.GetDormitoryTypesFilter();
+            return PartialView("_options", model);
+        }
+
+        public IActionResult GetDormitoriesFilter(int Id)
+        {
+            var model = _searchService.GetDormitoriesFilter(Id);
+            return PartialView("_options", model);
+        }
 
         public IActionResult GetSortingButtonSection()
         {
@@ -74,6 +93,7 @@ namespace searchDormWeb.Controllers
             return PartialView("_onScrollAlert", modelList[randomNumber]);
         }
     }
+
 
 
 
