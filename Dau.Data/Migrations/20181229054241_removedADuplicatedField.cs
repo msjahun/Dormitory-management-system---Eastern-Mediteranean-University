8a995f1a44f0b2be1efcaf2e6663f3658573cc7b
Musa Jahun <msjahun@live.com>
2018-12-29 08:25:12 +0200

    Added migration, removed duplicate roomQuota field

---
+++ Dau.Data/Migrations/20181229054241_removedADuplicatedField.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dau.Data.Migrations
+{
+    public partial class removedADuplicatedField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RoomsQuota",
+                table: "Room");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RoomsQuota",
+                table: "Room",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
