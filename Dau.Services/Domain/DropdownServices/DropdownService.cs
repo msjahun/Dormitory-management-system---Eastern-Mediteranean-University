89b6bb3f3a1e14e73991ed8ed49c159b2f4f4d30
Musa Jahun <msjahun@live.com>
2019-01-01 21:27:24 +0200

    added dropdown service for managing all dropdowns in the administrator panel

---
+++ Dau.Services/Domain/DropdownServices/DropdownService.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dau.Services.Domain.DropdownServices
+{
+    public class DropdownService : IDropdownService
+    {
+        public DropdownService()
+        {
+
+        }
+
+
+        //list of dormitoryBlocks
+        public List<SelectListItem> DormitoryBlocks()
+        {
+            var NorthAmericaGroup = new SelectListGroup { Name = "Europe" };
+            var EuropeGroup = new SelectListGroup { Name = "Europe" };
+
+          var  model = new List<SelectListItem>
+        {
+            new SelectListItem
+            {
+                Value ="1",
+                Text = "Mexico",
+                Group = NorthAmericaGroup
+            },
+            new SelectListItem
+            {
+                Value ="1",
+                Text = "Canada",
+                Group = NorthAmericaGroup
+            },
+            new SelectListItem
+            {
+                Value = "US",
+                Text = "USA",
+                Group = NorthAmericaGroup
+            },
+            new SelectListItem
+            {
+                Value = "2",
+                Text = "France",
+                Group = EuropeGroup
+            },
+            new SelectListItem
+            {
+                Value = "3",
+                Text = "Spain",
+                Group = EuropeGroup
+            },
+            new SelectListItem
+            {
+                Value = "4",
+                Text = "Germany",
+                Group = EuropeGroup
+            }
+      };
+
+            return model;
+        }
+
+    }
+
+
+
+}
---
+++ Dau.Services/Domain/DropdownServices/IDropdownService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Dau.Services.Domain.DropdownServices
+{
+    public interface IDropdownService
+    {
+        List<SelectListItem> DormitoryBlocks();
+    }
+}
