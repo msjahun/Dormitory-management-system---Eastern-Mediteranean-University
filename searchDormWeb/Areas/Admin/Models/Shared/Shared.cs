13ce259c4f39ba87b897af1f6ef0a0da0c1e65a7
Musa Jahun <msjahun@live.com>
2019-01-07 06:18:03 +0200

    moved SeoTab to dormitoryService

--- searchDormWeb/Areas/Admin/Models/Shared/Shared.cs
+++ searchDormWeb/Areas/Admin/Models/Shared/Shared.cs
@@ -6,27 +6,5 @@ using System.Threading.Tasks;
 
 namespace searchDormWeb.Areas.Admin.Models.Shared
 {
-    public class SeoTab
-    {
-
-        [Display(Name = "Meta Keywords",
-        Description = "Meta keywords to be added to dormitory page header."), DataType(DataType.Text), MaxLength(256)]
-        public string MetaKeywords {get; set;}
-
-
-        [Display(Name = "Meta Description",
-        Description = "Meta description to be added to dormitory page header."), DataType(DataType.Text), MaxLength(256)]
-        public string MetaDescription {get; set;}
-
-
-        [Display(Name = "Meta Title",
-        Description = "Override the page title. The default is the name of the dormitory."), DataType(DataType.Text), MaxLength(256)]
-        public string MetaTitle {get; set;}
-
-
-        [Display(Name = "Search Engine Friendly Page Name",
-        Description = "Set a search engine friendly page name e.g. 'the-best-dormitory' to make your page URL 'http://www.domain.com/the-best-dormitory'. Leave empty to generate it automatically based on the name of the Dormitory"), DataType(DataType.Text), MaxLength(256)]
-        public string SearchEngineFriendlyPageName { get; set; }
-
-    }
+   
 }
