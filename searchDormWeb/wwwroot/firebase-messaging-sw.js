6b6c3241af548c52eb46a339e4554e8390d03c35
Musa Jahun <msjahun@live.com>
2019-01-12 16:15:49 +0200

    setup service worker and push notification

---
+++ searchDormWeb/wwwroot/firebase-messaging-sw.js
@@ -0,0 +1,30 @@
+﻿// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+firebase.initializeApp({
+    'messagingSenderId': '980254597176'
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+
+messaging.setBackgroundMessageHandler(function (payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    var notificationTitle = payload.data.body;
+    var notificationOptions = {
+        body: payload.data.alertMsg,
+        icon: payload.data.icon,
+        image: payload.data.image
+    };
+
+    return self.registration.showNotification(notificationTitle,
+        notificationOptions);
+});
---
+++ searchDormWeb/wwwroot/serviceworker.js
@@ -0,0 +1,18 @@
+﻿console.log("Service Worker Loaded...");
+
+//self.addEventListener("push", e => {
+//    const data = e.data.json();
+//    console.log("Push Recieved...");
+//    self.registration.showNotification(data.title, {
+//        body: "Notified by Traversy Media!",
+//        icon: "http://image.ibb.co/frYOFd/tmlogo.png"
+//    });
+//});
+
+self.addEventListener('push', function (event) {
+    if (event.data) {
+        console.log('This push event has data: ', event.data.text());
+    } else {
+        console.log('This push event has no data.');
+    }
+});
