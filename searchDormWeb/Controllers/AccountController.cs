6391a815a8e712e75f9b534066d638e9ce05864f
Musa Jahun <msjahun@live.com>
2019-02-06 21:32:57 +0200

    Added authorize Dataannotation to account controller

--- searchDormWeb/Controllers/AccountController.cs
+++ searchDormWeb/Controllers/AccountController.cs
@@ -5,12 +5,14 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Dau.Services.Domain.BookingService;
 using Dau.Services.Utilities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace searchDormWeb.Controllers
 {
+    [Authorize]
     public class AccountController : Controller
     {
         private readonly IBookingService _bookingservice;
