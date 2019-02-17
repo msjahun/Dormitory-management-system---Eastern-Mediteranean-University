9ca297ad879350fbb38b1ae52c78774bccd307c8
Musa Jahun <msjahun@live.com>
2019-02-17 09:56:22 +0200

    removed string UserId from Online Users

--- Dau.Core/Domain/Users/OnlineUsers.cs
+++ Dau.Core/Domain/Users/OnlineUsers.cs
@@ -14,5 +14,6 @@ namespace Dau.Core.Domain.Users
 
         public string LastVisitedPage { get; set; }
         public DateTime LastActivityTime { get; set; }
+        public string UserId { get; set; }
     }
 }
