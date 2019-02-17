c5c5ffb4153361f5d5b03f482cc40331357bb3f2
Musa Jahun <msjahun@live.com>
2019-02-17 09:58:09 +0200

    made some changes to the AclServicesTests

--- Tests/Dau.Services.Tests/AclServices/AclServicesTest.cs
+++ Tests/Dau.Services.Tests/AclServices/AclServicesTest.cs
@@ -5,6 +5,7 @@ using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace Dau.Services.Tests.AclServices
 {
@@ -22,64 +23,49 @@ namespace Dau.Services.Tests.AclServices
 
         }
 
+    
+
         [Test]
         public void Parse_Access_Control_Json_Test()
         {
             string bodyStr =
-                "[{\"UserRole\":\"ADMINISTRATOR\",\"Area\":\"Admin\",\"Controller\":\"ActivityLog\",\"Action\":\"Log\"}," +
-                "{\"UserRole\":\"DORMITORYMANAGER\",\"Area\":\"Admin\",\"Controller\":\"ActivityLog\",\"Action\":\"Type\"}," +
-                "{\"UserRole\":\"ADMINISTRATOR\",\"Area\":\"Admin\",\"Controller\":\"API\",\"Action\":\"Settings\"}]";
+                "[{\"UserRole\":\"ADMINISTRATOR\",\"Area\":\"Admin\",\"Controller\":\"Debug\",\"Action\":\"Log\"}," +
+              
+                "{\"UserRole\":\"ADMINISTRATOR\",\"Controller\":\"Debug\",\"Action\":\"Log\"}]";
 
-           var mvcControllers = new List<AclMvcControllerInfo> {
+            var mvcControllers = new List<AclMvcControllerInfo> {
            new AclMvcControllerInfo{
            UserRole="ADMINISTRATOR",
            mvcControllers= new List<MvcControllerInfo>{
-               new MvcControllerInfo
+             
+                    new MvcControllerInfo
                {
-                   AreaName="Admin",
-                   Name="ActivityLog",
-                   Actions = new List<MvcActionInfo>
-                   {
-                       new MvcActionInfo
-                       {
-                           Name="Log"
-                       }
-                   }
-               },
-                  new MvcControllerInfo
-               {
-                   AreaName="Admin",
-                   Name="API",
+                   AreaName=null,
+                   Name="Debug",
                    DisplayName=null,
-                   
-                   Actions = new List<MvcActionInfo>
-                   {
-                       new MvcActionInfo
-                       {isSelected=true,
-                       ControllerId="Admin:ActivityLog:Log",
-                       DisplayName=null,
-                       
-                           Name="Settings"
-                       }
-                   }
-               },
-
-           }
-
-           },
-
-              new AclMvcControllerInfo{
-           UserRole="DORMITORYMANAGER",
-           mvcControllers= new List<MvcControllerInfo>{
-               new MvcControllerInfo
-               {
-                   AreaName="Admin",
-                   Name="ActivityLog",
                    Actions = new List<MvcActionInfo>
                    {
                        new MvcActionInfo
                        {
-                           Name="Type"
+                           Name="Log",
+                           DisplayName=null,
+                           ControllerId=":Debug"
+                       }
+                   }
+               },
+                      new MvcControllerInfo
+               {
+                   AreaName="Admin",
+                   Name="Debug",
+                   DisplayName=null,
+                   Actions = new List<MvcActionInfo>
+                   {
+                       new MvcActionInfo
+                       {
+                           Name="Log",
+                           DisplayName=null,
+                           ControllerId="Admin:Debug"
+
                        }
                    }
                }
@@ -88,9 +74,104 @@ namespace Dau.Services.Tests.AclServices
 
            }
 
+           
+           };
+
+
+            //  (1 + 1).ShouldEqual(3);
+            JsonConvert
+                   .SerializeObject(_userRolesService
+                   .ParseAccessControlJson(bodyStr))
+                   .ShouldEqual(JsonConvert.SerializeObject(mvcControllers));
+        }
+
+        [Test]
+        public void Parse_Access_Control_Json_With_Area_Public_Test()
+        {
+            string bodyStr =
+                "[{\"UserRole\":\"ADMINISTRATOR\",\"Area\":\"Public\",\"Controller\":\"Debug\",\"Action\":\"Log\"}]";
+
+            var mvcControllers = new List<AclMvcControllerInfo> {
+           new AclMvcControllerInfo{
+           UserRole="ADMINISTRATOR",
+           mvcControllers= new List<MvcControllerInfo>{
+               new MvcControllerInfo
+               {
+                   AreaName=null,
+                   Name="Debug",
+                   DisplayName=null,
+                   Actions = new List<MvcActionInfo>
+                   {
+                       new MvcActionInfo
+                       {
+                           Name="Log",
+                           DisplayName=null,
+                           ControllerId=":Debug"
+                       }
+                   }
+               }
+           }
+
+           }
+
 
            };
-            _userRolesService.ParseAccessControlJson(bodyStr).ShouldEqual(mvcControllers);
+
+
+            //  (1 + 1).ShouldEqual(3);
+            JsonConvert
+                   .SerializeObject(_userRolesService
+                   .ParseAccessControlJson(bodyStr))
+                   .ShouldEqual(JsonConvert.SerializeObject(mvcControllers));
         }
+
+
+
+
+        [Test]
+        public void Parse_Access_Control_Json_With_Null_Area_Test()
+        {
+            string bodyStr =
+                "[{\"UserRole\":\"DORMITORYMANAGER\",\"Controller\":\"Debug\",\"Action\":\"Log\"}]";
+
+            var mvcControllers = new List<AclMvcControllerInfo> {
+          
+
+              new AclMvcControllerInfo{
+           UserRole="DORMITORYMANAGER",
+           mvcControllers= new List<MvcControllerInfo>{
+               
+                    new MvcControllerInfo
+               {
+                   AreaName=null,
+                   Name="Debug",
+                   DisplayName=null,
+                   Actions = new List<MvcActionInfo>
+                   {
+                       new MvcActionInfo
+                       {
+                           Name="Log",
+                           DisplayName=null,
+                           ControllerId=":Debug"
+                       }
+                   }
+               }
+
+           }
+
+           }
+
+           };
+
+
+            //  (1 + 1).ShouldEqual(3);
+            JsonConvert
+                   .SerializeObject(_userRolesService
+                   .ParseAccessControlJson(bodyStr))
+                   .ShouldEqual(JsonConvert.SerializeObject(mvcControllers));
+        }
+
+  
+
     }
 }
